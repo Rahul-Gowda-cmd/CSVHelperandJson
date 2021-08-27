12adb11305db64aa3a685759a762a26315c3996e
HP <rahul.prabu.07@gmail.com>
2021-08-27 21:08:53 +0530

    Add project files.

---
+++ CSVHelperandJson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSVHelperandJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#Read Data from CSV and Write Data in CSV");
+            CSVHandler.ImplementCSVDataHandling();
+            Console.WriteLine();
+
+            Console.WriteLine("#Read Data from CSV and Write Data in Json");
+            ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+            Console.WriteLine();
+
+            Console.WriteLine("#Read Data from json and Write Data in CSV");
+            ReadJSONWriteCSV.ImplementJSONToCSV();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ CSVHelperandJson/ReadJSONWriteCSV.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CSVHelperandJson
+{
+    public class ReadJSONWriteCSV
+    {
+        public static void ImplementJSONToCSV()
+        {
+            string importFilePath = @"C:\Users\HP\csharp\CSVHelperandJson\CSVHelperandJson\Utility\export.json";
+            string exportFilePath = @"C:\Users\HP\csharp\CSVHelperandJson\CSVHelperandJson\Utility\export.csv";
+            IList<AddressData> addressData = JsonConvert.DeserializeObject<IList<AddressData>>(File.ReadAllText(importFilePath));
+
+            Console.WriteLine("**********************Reading from Json file and Write to csv file **************************");
+            using (var writer = new StreamWriter(exportFilePath))
+            using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csvExport.WriteRecords(addressData);
+            }
+
+        }
+    }
+}
